e68491d3a03a1b08b14762030a18c2f33b90317c
stuffbymax <96355058+stuffbymax@users.noreply.github.com>
2024-05-18 15:22:07 +0100

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# This script installs the XYZ software to the user's home directory using a local fetch script
+
+# Define variables
+SOFTWARE_NAME="XYZ" 
+FETCH_SCRIPT="./sfetch"  # Path to the local fetch script
+TEMP_DIR=$(mktemp -d)
+
+# Function to display messages
+display_message() {
+    echo "$1"
+}
+
+
+
+# Function to move fetch script to /usr/local/bin and set executable permissions
+setup_fetch_script() {
+    display_message "Setting up fetch script..."
+    chmod +x sfetch
+    sudo mv sfetch /usr/local/bin/sfetch
+    display_message "Setting executable permissions..."
+    sudo chmod +x /usr/local/bin/sfetch
+    display_message "Fetch script setup complete."
+}
+
+# Main function
+main() {
+    setup_fetch_script
+    
+}
+
+# Run the script
+main
